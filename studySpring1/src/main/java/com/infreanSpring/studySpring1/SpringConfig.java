14b83850c2335d9e607edc43fc016ea1c5104781
hailie <20181011@sungshin.ac.kr>
2021-02-17 00:43:13 +0900

    순수 jdbc

--- studySpring1/src/main/java/com/infreanSpring/studySpring1/SpringConfig.java
+++ studySpring1/src/main/java/com/infreanSpring/studySpring1/SpringConfig.java
@@ -1,13 +1,25 @@
 package com.infreanSpring.studySpring1;
 
+import com.infreanSpring.studySpring1.repository.JdbcMemberRepository;
 import com.infreanSpring.studySpring1.repository.MemberRepository;
 import com.infreanSpring.studySpring1.repository.MemoryMemberRepository;
 import com.infreanSpring.studySpring1.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -15,7 +27,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
         /*
         data를 Memory 저장에서 Db 저장으로 바꿀 시
         MemoryMemberRepository를 DBMemberRepository로 변환하면 된다.
