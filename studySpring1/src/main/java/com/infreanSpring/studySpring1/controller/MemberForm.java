a25058cc584f126db416bf6e41cf80a123b46855
hailie <20181011@sungshin.ac.kr>
2021-02-16 23:49:08 +0900

    회원 관리 - 웹 mvc 개발

---
+++ studySpring1/src/main/java/com/infreanSpring/studySpring1/controller/MemberForm.java
@@ -0,0 +1,16 @@
+package com.infreanSpring.studySpring1.controller;
+
+/*
+* 웹 등록 화면에서 데이터를 전달 받을 form 객체
+* */
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
