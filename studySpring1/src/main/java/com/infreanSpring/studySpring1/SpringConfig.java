2854278091396b3a9b812749149933417a063488
hailie <20181011@sungshin.ac.kr>
2021-02-17 20:55:02 +0900

    스프링 데이터 jpa

--- studySpring1/src/main/java/com/infreanSpring/studySpring1/SpringConfig.java
+++ studySpring1/src/main/java/com/infreanSpring/studySpring1/SpringConfig.java
@@ -12,27 +12,27 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-
+/*
     @Bean
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
         //return new JdbcTemplateMemberRepository(dataSource);
         return new JpaMemberRepository(em);
-        /*
-        data를 Memory 저장에서 Db 저장으로 바꿀 시
-        MemoryMemberRepository를 DBMemberRepository로 변환하면 된다.
-        * */
+
+        //data를 Memory 저장에서 Db 저장으로 바꿀 시
+        //MemoryMemberRepository를 DBMemberRepository로 변환하면 된다.
+
     }
+*/
 }
